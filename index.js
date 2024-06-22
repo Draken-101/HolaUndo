6ab7fcb01949d49f08aff1cede59a52594f84f0f
Draken-101 <leonardofavion@gmail.com>
2024-06-21 19:13:12 -0600

    Hola Undo

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(cors({
 }));
 const port = 3000;
 app.get(`/api/v1/welcome`, (req, res) => {
-    res.send(`Hola Undo`);
+    res.send(`Hola Undos`);
 });
 app.listen(port, () => {
     console.log(`Server is running on: http://localhost:${port}`);
